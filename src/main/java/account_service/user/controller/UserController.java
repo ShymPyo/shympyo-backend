10f8d4351f2132d58969c3db4585fc2a2b136747
bmh7190 <bmh7190@naver.com>
2025-08-26 05:47:05 +0900

    refactor: 공통응답패턴 컨트롤러 적용

--- src/main/java/account_service/user/controller/UserController.java
+++ src/main/java/account_service/user/controller/UserController.java
@@ -24,18 +24,20 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<CommonResponse<TokenResponse>> signup(@RequestBody @Valid SignUpRequest request){
+
         TokenResponse token = userService.signUp(request);
 
-        return ResponseEntity.ok(ResponseUtil.success("회원가입이 완료 되었습니다.", token));
+        return ResponseUtil.success("회원가입이 완료되었습니다.",token);
     }
 
 
     @GetMapping("/me")
     public ResponseEntity<CommonResponse<UserInfoResponse>> getMyInfo(@AuthenticationPrincipal CustomUserDetails userDetails){
+
         Long userId = userDetails.getId();
         UserInfoResponse userInfo = userService.getMyInfo(userId);
 
-        return ResponseEntity.ok(ResponseUtil.success(userInfo));
+        return ResponseUtil.success(userInfo);
     }
 
     @PatchMapping("/me")
@@ -44,16 +46,12 @@ public class UserController {
             @RequestBody UpdateUserRequest request){
 
         if(request.isEmpty()){
-            return ResponseEntity.badRequest().body(ResponseUtil.fail("수정할 필드를 하나 이상 입력해야 합니다."));
+            return ResponseUtil.fail("수정할 필드를 하나 이상 입력해야 합니다.");
         }
 
         UserInfoResponse updated = userService.updateUserInfo(userDetails.getId(), request);
-        return ResponseEntity.ok(ResponseUtil.success("회원 정보 수정 완료",updated));
+
+        return ResponseUtil.success("회원 정보 수정 완료",updated);
     }
 
-//    @PostMapping("/oauth")
-//    public ResponseEntity<Long> findOrCreateUser(@RequestBody KakaoUserInfo userInfo) {
-//        Long userId = userService.findOrCreateByEmail(userInfo);
-//        return ResponseEntity.ok(userId);
-//    }
 }
