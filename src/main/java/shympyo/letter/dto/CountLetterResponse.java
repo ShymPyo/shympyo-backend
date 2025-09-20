9d65dc5bc4279eea2617542646e8e04f3aa16db6
bmh7190 <bmh7190@naver.com>
2025-09-20 16:25:16 +0900

    chore: 편지 서비스 Swagger 적용

--- src/main/java/shympyo/letter/dto/CountLetterResponse.java
+++ src/main/java/shympyo/letter/dto/CountLetterResponse.java
@@ -1,14 +1,20 @@
 package shympyo.letter.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @AllArgsConstructor
 @Getter
+@Schema(description = "받은 편지 개수 응답 DTO")
 public class CountLetterResponse {
 
+    @Schema(description = "총 받은 편지 개수", example = "42")
     private Long total;
-    private Long unRead;
-    private Long read;
 
+    @Schema(description = "읽지 않은 편지 개수", example = "10")
+    private Long unRead;
+
+    @Schema(description = "읽은 편지 개수", example = "32")
+    private Long read;
 }
--- src/main/java/shympyo/letter/dto/WriterInfo.java
+++ src/main/java/shympyo/letter/dto/WriterInfo.java
@@ -1,5 +1,6 @@
 package shympyo.letter.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -7,11 +8,19 @@ import lombok.Getter;
 @Getter
 @Builder
 @AllArgsConstructor
+@Schema(description = "편지 작성자 정보 DTO")
 public class WriterInfo {
 
+    @Schema(description = "작성자 ID", example = "77")
     private Long id;
+
+    @Schema(description = "작성자 이름", example = "홍길동")
     private String name;
+
+    @Schema(description = "작성자 이메일", example = "hong@example.com")
     private String email;
+
+    @Schema(description = "작성자 전화번호", example = "010-1234-5678")
     private String phone;
 
 }
