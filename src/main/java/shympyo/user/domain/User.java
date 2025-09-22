b013772a9f2f5c22170e3ea2204f016dbc8c545e
bmh7190 <bmh7190@naver.com>
2025-09-22 17:09:58 +0900

    refactor: 닉네임 필드와 프로필 사진 필드 추가

--- src/main/java/shympyo/user/domain/User.java
+++ src/main/java/shympyo/user/domain/User.java
@@ -23,13 +23,18 @@ public class User {
     @Column(nullable = false, length = 50)
     private String name;
 
+    @Column(nullable = true, length = 50)
+    private String nickname;
+
     @Column(nullable = true)
     private String phone;
+
+    @Column(nullable = true)
+    private String imageUrl;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, length = 20)
     private UserRole role;
-
 
 
 }
