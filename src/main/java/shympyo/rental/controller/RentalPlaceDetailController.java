78548b4ed0317982486f8e8401a089647439817f
bmh7190 <bmh7190@naver.com>
2025-10-31 00:33:31 +0900

    feat: 사용자의 위치 기반 컨트롤러 추가

--- src/main/java/shympyo/rental/controller/RentalPlaceDetailController.java
+++ src/main/java/shympyo/rental/controller/RentalPlaceDetailController.java
@@ -2,10 +2,12 @@ package shympyo.rental.controller;
 
 import java.time.LocalDateTime;
 
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
 import shympyo.rental.dto.external.RentalPlaceDetailDto;
+import shympyo.rental.dto.external.RentalPlaceDto;
 import shympyo.rental.service.RentalMapQueryService;
 
 @RestController
@@ -23,5 +25,24 @@ public class RentalPlaceDetailController {
             LocalDateTime at
     ) {
         return rentalMapQueryService.getPlaceDetail(placeId, at);
+    }
+
+    @GetMapping("/box")
+    public List<RentalPlaceDto> getPlacesInBox(
+            @RequestParam double minLat,
+            @RequestParam double maxLat,
+            @RequestParam double minLon,
+            @RequestParam double maxLon,
+            @RequestParam Long userId,
+            @RequestParam
+            @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME)
+            LocalDateTime at
+    ) {
+        return rentalMapQueryService.findUserSheltersInBox(
+                minLat, maxLat,
+                minLon, maxLon,
+                userId,
+                at
+        );
     }
 }
